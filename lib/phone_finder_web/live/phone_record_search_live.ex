ccaaafe1d54361869a7d6588172d5e96c6d8d942
Jason Kolios <jason.kolios@gmail.com>
2021-10-09 16:53:10 +0300

    Only start live searches with >2 chars

--- lib/phone_finder_web/live/phone_record_search_live.ex
+++ lib/phone_finder_web/live/phone_record_search_live.ex
@@ -2,28 +2,22 @@ defmodule PhoneFinderWeb.PhoneRecordSearchLive do
   use PhoneFinderWeb, :live_view
 
   def mount(_params, %{}, socket) do
-    if connected?(socket), do: Process.send_after(self(), :update, 3000)
     socket =
       socket
       |> assign(:search_term, "")
-      |> assign(:time, DateTime.utc_now)
       |> assign(:search_results, %{})
     {:ok, socket}
   end
 
   def handle_event("search_term_change", value, socket) do
-    socket =
-      socket
-      |> assign(:search_term, value["search_term"]["term"])
-      |> assign(:time, DateTime.utc_now)
-      |> assign(:search_results, PhoneFinder.Phones.search_phone_records(value["search_term"]["term"]))
-    {:noreply, socket}
-  end
-
-  def handle_info(:update, socket) do
-    Process.send_after(self(), :update, 3000)
+    search_results = if String.length(value["search_term"]["term"]) > 2 do
+      PhoneFinder.Phones.search_phone_records(value["search_term"]["term"])
+    else
+      []
+    end
     socket = socket
-    |> assign(:time, DateTime.utc_now)
+      |> assign(:search_results, search_results)
+      |> assign(:search_term, value["search_term"]["term"])
     {:noreply, socket}
   end
 end
